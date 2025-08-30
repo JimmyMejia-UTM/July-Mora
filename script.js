ffc007ffd3b8134ec135fe69c532af40c5c15afc
JimmyMejia-UTM <emejia0482@utm.edu.ec>
2025-08-30 01:37:31 -0500

    July Mora

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-var radius = 240; 
-var autoRotate = true; 
-var rotateSpeed = -60; 
-var imgWidth = 200; 
-var imgHeight = 300; 
-
-var bgMusicURL = './july.mp3';
-var bgMusicControls = true; 
-
-setTimeout(init, 1000);
+var radius = 280; // menos separación
+var autoRotate = true;
+var rotateSpeed = 20; // velocidad rotación
+var imgWidth = 220;
+var imgHeight = 300;
 
 var odrag = document.getElementById('drag-container');
 var ospin = document.getElementById('spin-container');
 var aImg = ospin.getElementsByTagName('img');
-var aEle = [...aImg]; 
+var aEle = [...aImg];
 
 ospin.style.width = imgWidth + "px";
 ospin.style.height = imgHeight + "px";
@@ -21,81 +16,71 @@ var ground = document.getElementById('ground');
 ground.style.width = radius * 3 + "px";
 ground.style.height = radius * 3 + "px";
 
-function init(delayTime) {
-  for (var i = 0; i < aEle.length; i++) {
-    aEle[i].style.transform = "rotateY(" + (i * (360 / aEle.length)) + "deg) translateZ(" + radius + "px)";
-    aEle[i].style.transition = "transform 1s";
-    aEle[i].style.transitionDelay = delayTime || (aEle.length - i) / 4 + "s";
+var tX = 0, tY = 10, desX = 0, desY = 0, sX, sY, nX, nY;
+
+function init(delayTime){
+  for(var i=0;i<aEle.length;i++){
+    aEle[i].style.transform = `rotateY(${i*(360/aEle.length)}deg) translateZ(${radius}px)`;
+    aEle[i].style.transition = `transform 1s`;
+    aEle[i].style.transitionDelay = (delayTime || (aEle.length-i)/4) + 's';
   }
 }
 
-function applyTranform(obj) {
-  if(tY > 180) tY = 180;
-  if(tY < 0) tY = 0;
-  obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
+function applyTransform(obj){
+  if(tY>180) tY=180;
+  if(tY<0) tY=0;
+  obj.style.transform = `rotateX(${-tY}deg) rotateY(${tX}deg)`;
 }
 
-function playSpin(yes) {
-  ospin.style.animationPlayState = (yes?'running':'paused');
+function playSpin(yes){
+  ospin.style.animationPlayState = yes?'running':'paused';
 }
 
-var sX, sY, nX, nY, desX = 0,
-    desY = 0,
-    tX = 0,
-    tY = 10;
-
-if (autoRotate) {
-  var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
-  ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
+// Animación rotación automática
+if(autoRotate){
+  let deg = 0;
+  setInterval(()=>{
+    deg += rotateSpeed * 0.017;
+    tX = deg;
+    applyTransform(odrag);
+  },17);
 }
 
-if (bgMusicURL) {
-  document.getElementById('music-container').innerHTML = `
-    <audio src="${bgMusicURL}" ${bgMusicControls ? 'controls' : ''} autoplay loop></audio>
-  `;
-}
-
-document.onpointerdown = function (e) {
+// Drag
+document.onpointerdown = function(e){
   clearInterval(odrag.timer);
-  e = e || window.event;
-  var sX = e.clientX,
-      sY = e.clientY;
+  sX = e.clientX; sY = e.clientY;
 
-  this.onpointermove = function (e) {
-    e = e || window.event;
-    var nX = e.clientX,
-        nY = e.clientY;
+  this.onpointermove = function(e){
+    nX = e.clientX; nY = e.clientY;
     desX = nX - sX;
     desY = nY - sY;
-    tX += desX * 0.1;
-    tY += desY * 0.1;
-    applyTranform(odrag);
-    sX = nX;
-    sY = nY;
-  };
+    tX += desX*0.1;
+    tY += desY*0.1;
+    applyTransform(odrag);
+    sX = nX; sY = nY;
+  }
 
-  this.onpointerup = function (e) {
-    odrag.timer = setInterval(function () {
-      desX *= 0.95;
-      desY *= 0.95;
-      tX += desX * 0.1;
-      tY += desY * 0.1;
-      applyTranform(odrag);
+  this.onpointerup = function(e){
+    odrag.timer = setInterval(()=>{
+      desX *= 0.95; desY *= 0.95;
+      tX += desX*0.1; tY += desY*0.1;
+      applyTransform(odrag);
       playSpin(false);
-      if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
+      if(Math.abs(desX)<0.5 && Math.abs(desY)<0.5){
         clearInterval(odrag.timer);
         playSpin(true);
       }
-    }, 17);
+    },17);
     this.onpointermove = this.onpointerup = null;
-  };
-
+  }
   return false;
-};
+}
 
-document.onmousewheel = function(e) {
-  e = e || window.event;
-  var d = e.wheelDelta / 20 || -e.detail;
+document.onmousewheel = function(e){
+  var d = e.wheelDelta/20 || -e.detail;
   radius += d;
   init(1);
 };
+
+init();
